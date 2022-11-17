22c02541135f049b3e2079136d2c17ea73f52916
v_hllhliang <v_hllhliang@tencent.com>
2022-11-17 16:56:05 +0800

    装饰器模式

---
+++ structural-patterns/decorator/run_test.go
@@ -0,0 +1,20 @@
+package decorator
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	pizza := &VeggieMania{}
+
+	pizzaWithCheese := &CheeseTopping{
+		pizza: pizza,
+	}
+
+	pizzaWithCheeseAndTomato := &TomatoTopping{
+		pizza: pizzaWithCheese,
+	}
+
+	fmt.Printf("Price of veggeMania with tomato and cheese topping is %d\n", pizzaWithCheeseAndTomato.getPrice())
+}
